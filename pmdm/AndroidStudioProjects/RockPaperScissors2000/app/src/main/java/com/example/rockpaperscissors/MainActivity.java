c9600e3f0fb0ab2ba0630d36bd35d63f95576ef4
ianharrisonromero <ian.harrisonromero@educa.madrid.org>
2023-10-18 20:31:06 +0200

    algo de C (llamadas, md5) y SGE (funcion flecha)

--- pmdm/AndroidStudioProjects/RockPaperScissors2000/app/src/main/java/com/example/rockpaperscissors/MainActivity.java
+++ pmdm/AndroidStudioProjects/RockPaperScissors2000/app/src/main/java/com/example/rockpaperscissors/MainActivity.java
@@ -8,16 +8,89 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btRestart;
     ImageView ivRock, ivRockCpu, ivPaper, ivPaperCpu, ivScissors, ivScissorsCpu;
-    TextView tvUser, tvCpu, tvScore, tvUserScore, tvCpuScore, tvTitle, tvCpuChose,tvResultWin, tvResultLost;
+    TextView tvUser, tvCpu, tvUserScore, tvCpuScore, tvTitle, tvCpuChose,tvResultWin, tvResultLost, tvResultDraw;
+
+    private final int INITIAL_SCORE=0, FINAL_SCORE=0;
+    boolean gameEnded = false;
+
+    //ENUMS
     public enum Selection{
         ROCK, PAPER, SCISSORS;
     }
 
+    private void setResultsGone(){
+        tvResultLost.setVisibility(View.GONE);
+        tvResultWin.setVisibility(View.GONE);
+        tvResultDraw.setVisibility(View.GONE);
+    }
+    View.OnClickListener OnRPSClick = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if (!gameEnded) {
+                setResultsGone();
+                Selection CPUSelection = CPURandomSelection();
+                switch ((Selection) v.getTag()) {
+                    case ROCK:
+                        if (CPUSelection == Selection.ROCK) {
+                            tvResultDraw.setVisibility(View.VISIBLE);
+                        } else if (CPUSelection == Selection.SCISSORS) {
+                            tvUserScore.setText(String.valueOf(Integer.parseInt(tvUserScore.getText().toString()) + 1));
+                            tvResultWin.setVisibility(View.VISIBLE);
+                        } else if (CPUSelection == Selection.PAPER) {
+                            tvCpuScore.setText(String.valueOf(Integer.parseInt(tvCpuScore.getText().toString()) + 1));
+                            tvResultLost.setVisibility(View.VISIBLE);
+                        }
+                        break;
+                    case PAPER:
+                        if (CPUSelection == Selection.PAPER) {
+                            tvResultDraw.setVisibility(View.VISIBLE);
+                        } else if (CPUSelection == Selection.ROCK) {
+                            tvUserScore.setText(String.valueOf(Integer.parseInt(tvUserScore.getText().toString()) + 1));
+                            tvResultWin.setVisibility(View.VISIBLE);
+                        } else if (CPUSelection == Selection.SCISSORS) {
+                            tvCpuScore.setText(String.valueOf(Integer.parseInt(tvCpuScore.getText().toString()) + 1));
+                            tvResultLost.setVisibility(View.VISIBLE);
+                        }
+                        break;
+                    case SCISSORS:
+                        if (CPUSelection == Selection.SCISSORS) {
+                            tvResultDraw.setVisibility(View.VISIBLE);
+                        } else if (CPUSelection == Selection.PAPER) {
+                            tvUserScore.setText(String.valueOf(Integer.parseInt(tvUserScore.getText().toString()) + 1));
+                            tvResultWin.setVisibility(View.VISIBLE);
+                        } else if (CPUSelection == Selection.ROCK) {
+                            tvCpuScore.setText(String.valueOf(Integer.parseInt(tvCpuScore.getText().toString()) + 1));
+                            tvResultLost.setVisibility(View.VISIBLE);
+                        }
+                        break;
+                }
+
+                if (Integer.parseInt(tvUserScore.getText().toString())==3 || Integer.parseInt(tvCpuScore.getText().toString())==3){
+                    gameEnded = true;
+                }
+
+            }
+        }
+    };
+
+    private Selection CPURandomSelection(){
+        Random random = new Random();
+        int randomNum = random.nextInt(3);
+
+        switch (randomNum){
+            case 0 : return Selection.ROCK;
+            case 1 : return Selection.PAPER;
+            case 2 : return  Selection.SCISSORS;
+            default: return Selection.ROCK; //Justin Case
+        }
+    }
 
 
     @Override
@@ -38,25 +111,36 @@ public class MainActivity extends AppCompatActivity {
         tvCpu = findViewById(R.id.tvCpu);
         tvCpuScore = findViewById(R.id.tvCpuScore);
         tvCpuChose = findViewById(R.id.tvCpuChose);
-        tvScore = findViewById(R.id.tvScore);
         tvResultWin = findViewById(R.id.tvResultWin);
         tvResultLost = findViewById(R.id.tvResultLost);
-        //invisibles
+        tvResultDraw = findViewById(R.id.tvResultDraw);
+        //invisibles/gone
         tvCpuChose.setVisibility(View.GONE);
         ivRockCpu.setVisibility(View.GONE);
         ivPaperCpu.setVisibility(View.GONE);
         ivScissorsCpu.setVisibility(View.GONE);
         tvResultLost.setVisibility(View.GONE);
         tvResultWin.setVisibility(View.GONE);
+        tvResultDraw.setVisibility(View.GONE);
 
-        ivRock.setOnClickListener(v -> {
+        //set on RPS click listener
+        ivRock.setOnClickListener(OnRPSClick);
+        ivPaper.setOnClickListener(OnRPSClick);
+        ivScissors.setOnClickListener(OnRPSClick);
 
+        //setting tags
+        ivRock.setTag(Selection.ROCK);
+        ivPaper.setTag(Selection.PAPER);
+        ivScissors.setTag(Selection.SCISSORS);
 
+        //restart button
+        btRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResultsGone();
+                tvCpuScore.setText(INITIAL_SCORE);
+                tvUserScore.setText(INITIAL_SCORE);
+            }
         });
-
-
-
-
-
     }
 }
