e21f6afb798a687314b70ff16ecedcbc1ce92990
ianharrisonromero <ian.harrisonromero@educa.madrid.org>
2024-01-07 16:09:14 +0100

    empezando tareas UDP

---
+++ pmdm/AndroidStudioProjects/RecyclerViewsActivity/app/src/main/java/com/example/recyclerviewsactivity/MyAdapter.java
@@ -0,0 +1,80 @@
+package com.example.recyclerviewsactivity;
+import java.util.ArrayList;
+import java.util.Arrays;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private ArrayList<ut03Partido> datos;
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView partido;
+        private final TextView resultado;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            partido = (TextView) view.findViewById(R.id.tvNombre);
+            resultado = (TextView) view.findViewById(R.id.tvResultado);
+        }
+
+        public TextView getTextPartido() {
+            return partido;
+        }
+
+        public TextView getTextResultado() {
+            return resultado;
+        }
+    }
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *
+     * @param dataSet String[] containing the data to populate views to be used
+     * by RecyclerView.
+     */
+    public MyAdapter(ut03Partido[] dataSet) {
+        datos = new ArrayList<ut03Partido>();
+        add(dataSet);
+    }
+
+    // Create new views (invoked by the layotut manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.adapter_layout, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.getTextPartido().setText(datos.get(position).local + " vs " + datos.get(position).visitante);
+        viewHolder.getTextResultado().setText(datos.get(position).resultado);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return datos.size();
+    }
+
+    public void add(ut03Partido[] dataSet){
+        datos.addAll(Arrays.asList(dataSet));
+        notifyDataSetChanged();
+    }
+}
---
+++ psp/ut03/tareaUDP/tarea1/client1.java
@@ -0,0 +1,50 @@
+public class client1 {
+
+    /**
+     * Tarea 1: Ejemplo simple
+     * 
+     * Crea un programa servidor que reciba por parámetro un número que representa
+     * el puerto en el que escuchará, cuando reciba un mensaje lo escribirá por
+     * pantalla.
+     * Crea un programa cliente que reciba por parámetro una cadena representando la
+     * dirección ip, un número representando el puerto y una última cadena
+     * representando el texto a enviar. El programa enviará la información usando
+     * UDP.
+     */
+
+    private static final int MAX_LENGTH = 65535;
+
+    public static void main(String[] args) {
+
+        int port;
+
+        if (args.length != 3) {
+            System.out.println("ERROR: se necesitan tres parámetros (IP, puerto y mensaje)\n");
+            return;
+        }
+
+        try {
+            port = Integer.parseInt(args[1]);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            InetAddress ipAddress = args[0];
+            byte[] sendData = new byte[MAX_LENGTH];
+            String sentence = ""; // Mensaje a enviar
+            // TODO : un while para que le puedas enviar al server todos los mensajes que
+            // quieras.
+
+            sendData = sentence.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddress, port);
+            socket.send(sendPacket); // Envía el paquete al servidor
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
